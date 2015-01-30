d1ff2aca253fdfac6646451abf75cfcd904a1879
Josh Feck <josh@nxnweb.com>
2015-01-30 17:31:30 -0500

    add a few tweaks for the date times display, archives, single event and venue posts, remove default widgets if no widgets are set, and add a screenshot.

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package ThemeGrill
+ * @subpackage Radiate
+ * @since Radiate 1.0
+ */
+?>
+	<div id="secondary" class="widget-area" role="complementary">
+		<?php do_action( 'before_sidebar' ); ?>
+		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		<?php endif; // end sidebar widget area ?>
+	</div><!-- #secondary -->
